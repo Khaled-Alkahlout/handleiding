3702267362cc3c2cc0a8167a8d528f41c6c90418
sys72740hro <Hcbdag@635>
2022-10-03 14:21:12 +0200

    PHP CODE en rename index.html naar index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
+<?php
+
+$name = $_GET['name'];
+$title = $_GET['title'];
+
+echo "
 <!DOCTYPE html>
-<html lang="">
+<html lang=\"\">
   <head>
-    <meta charset="utf-8">
-    <title>Github voorbeeld</title>
+    <meta charset=\"utf-8\">
+    <title>$title</title>
   </head>
   <body>
-    <h1>Welkom op deze webapp Github demo<?php echo $_GET['name'];?></h1>  <!--TODO php activate in apache-->
+    <h1>Welkom op deze webapp Github demo $name</h1>
     <nav></nav>
     <header></header>
     <main></main>
     <footer></footer>
+    <form action='index.php' method='get'>
+          <input type=\"text\" id=\"name\" name=\"name\" placeholder=\"Vul uw naam in\" value=\"$name\"><br>
+          <input type=\"text\" id=\"title\" name=\"title\" placeholder=\"Voer de titel in\" value=\"$title\"><br><br>
+          <input type=\"submit\" value=\"Submit\">
+    </form>
   </body>
 </html>
-
-
-<?php
+";
 
 // om de bovenstaande code goed te gebruiken moet je apache van Xamp opstarten en deze URL invoeren:
 // http://localhost/Gouda_SWD1/index.php?name= van Henry
